687ec1f6f1ce8fc2a3c3cb3453115b3d80863fae
WilliIB <willi.imgruth@gmail.com>
2024-01-02 17:12:03 +0100

    Module 2 chapter 3.6

--- show-modal.js
+++
@@ -1,2 +0,0 @@
-// document.getElementById("new-project-modal").showModal()
-// document.getElementById("new-user-modal").showModal()
---
+++ src/classes/ProjectsManager.ts
@@ -0,0 +1,18 @@
+import { IProject, Project } from "./Project";
+
+export class ProjectsManager {
+   list: Project[] = [];
+   ui: HTMLElement;
+
+   constructor(container:HTMLElement) {
+    this.ui = container
+   }
+
+   newProject(data: IProject) {
+      const project = new Project(data);
+      this.ui.append(project.ui)
+      this.list.push(project);
+      return project;
+   }
+
+}
