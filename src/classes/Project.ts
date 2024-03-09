f8e539ad80eab3d343d0fb6fcfe9a0f9e084ebd9
Willi <willi.imgruth@gmail.com>
2024-03-09 21:40:03 +0100

    set project initials

--- src/classes/Project.ts
+++ src/classes/Project.ts
@@ -20,16 +20,17 @@ export class Project implements IProject {
 
   ui: HTMLDivElement;
   id: string;
+  initials: string;
   cost: number = 0;
   progress: number = 0;
 
   constructor(data: IProject) {
     this.id = uuidv4();
-    
+
     for (const key in data) {
       this[key] = data[key];
     }
-
+    this.setInitials();
     this.setUi();
   }
 
@@ -40,7 +41,7 @@ export class Project implements IProject {
     this.ui = document.createElement("div");
     this.ui.className = "project-card";
     this.ui.innerHTML = `<div class="card-header">
-      <p style="background-color: #ca8134; padding: 10px; border-radius: 8px; aspect-ratio: 1;">HC</p>
+      <p style="background-color: #ca8134; padding: 10px; border-radius: 8px; aspect-ratio: 1;">${this.initials}</p>
       <div>
         <h5>${this.name}</h5>
         <p>${this.description}</p>
@@ -64,5 +65,18 @@ export class Project implements IProject {
         <p>${this.progress}</p>
       </div>
     </div>`;
+  }
+
+  setInitials() {
+    const words = this.name.split(" ");
+    if (words.length > 1) {
+      const firstLetter = words[0].slice(0, 1);
+      const secondLetter = words[1].slice(0, 1);
+      const initials = firstLetter + secondLetter;
+      this.initials = initials.toUpperCase();
+    } else {
+      const initials = this.name.slice(0, 2);
+      this.initials = initials.toUpperCase();
+    }
   }
 }
